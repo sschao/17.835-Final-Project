cc8d983dd5883e7d281c0144daf054f5a66bc15b
Sichen (Shawn) Chao <shawns.chao@gmail.com>
2020-05-05 18:52:36 -0500

    Update affil_matrix_evan.py
    Fixed the bugs in previous affil_matrix_evan.py code

--- affil_matrix_evan.py
+++ affil_matrix_evan.py
@@ -9,13 +9,13 @@ import random
 
 path = os.path.abspath(os.getcwd()) +"\\candidate_follower_usernames" #path of original datafiles
 file_names = [f for f in listdir(path) if isfile(join(path, f))] #file names in path
-candidate_usernames = [file_name[:-4] for file_name in file_names] #take out csvs for twitter handles
+candidate_usernames = [file_name[:-4].lower() for file_name in file_names] #take out csvs for twitter handles
 usernames_df = pd.DataFrame()
 for file_name in file_names:
     df_candidate_username = pd.read_csv(os.path.abspath(os.getcwd()) +"\\candidate_follower_usernames\\"+file_name)
     usernames_df = pd.concat([usernames_df,df_candidate_username]) #merge all dataframes of usernames together
 
-
+df_sanders_followers= pd.read_csv(os.path.abspath(os.getcwd()) +"\\candidate_follower_usernames\\berniesanders.csv")
 # print(twitter_usernames)
 # test = pd.read_pickle('Followers of Candidates/ewarren.pkl')
 # print(len(test['username'].unique()))
@@ -44,14 +44,13 @@ def following(username, num_following):
     list_of_following = Following_df['following'][username]
     return list_of_following
 
-affil.to_csv('affiliation_matrix_0.csv') #initial csv
+affil.to_csv('affiliation_matrix_lower_evan_0.csv') #initial csv
 
 
 success = 0
-# affil_csv = pd.read_csv(f'affiliation_matrix_chao_{str(success)}.csv', index_col = 0)
+affil_csv = pd.read_csv(f'affiliation_matrix_lower_evan_{str(success)}.csv', index_col = 0)
 # affil = affil_csv[0:33000] #Shawn's work
-# affil = affil_csv[33000:] #Evan uncomment
-affil = affil[33000:]
+affil = affil_csv[33000:] #Evan uncomment
 usernames_index_list = list(affil.index)
 # original_length = len(usernames_index_list)
 # for i in range(len(usernames_index_list)):
@@ -60,11 +59,14 @@ while len(usernames_index_list) != 0:
     user = usernames_index_list.pop(random.randint(0,len(usernames_index_list)-1)) #pop out the user
     try:
         if affil.loc[user].sum() == 0:
-             # user = usernames_index_list.pop(0)
+            # if user in list(df_sanders_followers['username']):
+            #     print(user, "Sanders Follower")
+            #     break
             followings_list = following(user, 5000)
-            for followings in followings_list:
+            followings_list_lower = map(lambda x: x.lower(), followings_list)
+            for followings in followings_list_lower:
                 if followings in candidate_usernames:
-                    affil.loc[user][followings] += 1
+                    affil.loc[user][followings] = 1
 
             # print(user)
             # affil.to_csv(f'affiliation_matrix_{str(affil.index.get_loc(user))}.csv')
@@ -74,8 +76,8 @@ while len(usernames_index_list) != 0:
             # affil.to_csv(f'affiliation_matrix_{str(original_length-len(usernames_index_list))}.csv')
             # os.remove(f'affiliation_matrix_{str(original_length-len(usernames_index_list)-1)}.csv')
             success += 1
-            affil.to_csv(f'affiliation_matrix_evan_{str(success)}.csv') #new file
-            os.remove(f'affiliation_matrix_evan_{str(success-1)}.csv') #remove old file
+            affil.to_csv(f'affiliation_matrix_lower_evan_{str(success)}.csv') #new file
+            os.remove(f'affiliation_matrix_lower_evan_{str(success-1)}.csv') #remove old file
 
     except:
         usernames_index_list.append(user) #add the user back in if we failed
